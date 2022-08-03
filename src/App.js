ad2635d238b7fbf5d73fb9f9828ed95266201c44
Jackson <technologyj4ck@gmail.com>
2022-08-03 17:22:06 +0530

    day-2 third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     {
@@ -24,7 +25,7 @@ const App = () => {
   ];
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NewExpense/>
       <Expenses items = {expenses}/>
     </div>
   );
